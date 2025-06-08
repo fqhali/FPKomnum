14a82691b88c98115b8acb325f99a07deee79c2e
fqhali <faqihalirbn@gmail.com>
2025-06-08 18:58:51 +0700

    Add files via upload

---
+++ fp.py
@@ -0,0 +1,55 @@
+import numpy
+import sympy
+
+# integralnya -> f(x) = (4/5)*x**5-4*x**3 sebagai input
+# x0 = 2
+# x3 = 11
+# h = 3
+
+def errortrue(f_value, y_values):
+    return abs((f_value-y_values)/f_value)
+
+def heun_method (f, x0, x3, h):
+    x = sympy.symbols('x')
+    f_derivative = sympy.diff(f, x)
+    print(f"\nf(x, y) = {f_derivative}\n")
+    xn = x0
+    step = 3
+    
+    for i in range(step):
+        f_value = f.subs(x, xn)
+        f_valueup = f.subs(x, xn+h)
+        f_derivative_value = f_derivative.subs(x, xn)
+        f_derivative_valueup = f_derivative.subs(x, xn+h)
+        if(i == 0 ):
+            print(f"Step 1:\nx0 = {xn}\ty0 = {f_value}\n")
+            #print(f"{f_derivative_value}")
+            #print(f"{f_derivative_valueup}")
+            #print(f"{f_value}")
+
+            y_values = f_value + (((f_derivative_value+f_derivative_valueup)/2)*h)
+            xn = xn + h
+            print(f"x0 = {xn}\ty1 = {y_values}\n")
+            error = round(errortrue(f_valueup, y_values), 2)
+            print(f"\t\tEt = {error}")
+            continue
+        y_values = y_values + (((f_derivative_value+f_derivative_valueup)/2)*h)
+        xn = xn + h
+        print(f"Step {i+1}:\nx0 = {xn}\ty1 = {y_values}\n")
+        #print(f"{f_value}")
+        error = round(errortrue(f_valueup, y_values), 2)
+        print(f"\t\tEt = {error}")
+
+
+    
+def main():
+    x = sympy.symbols('x')
+    f = input("Enter the function f(x) in terms of x: ")
+    f = sympy.sympify(f)
+    x0 = float(input("Enter x0: "))
+    x3 = float(input("Enter x3: "))
+    h = float(input("Enter step h: "))
+    heun_method(f, x0, x3, h)
+
+if __name__ == "__main__":
+    main()
